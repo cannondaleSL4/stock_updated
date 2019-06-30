076baddd605c7ca58b2f3112065203ee4deb015a
Dmitriy <workday2006@yandex.ru>
2019-06-30 15:36:40 +0300

    init

--- rewrite.py
+++ rewrite.py
@@ -7,7 +7,7 @@ def rewrite():
     parsed_html = BeautifulSoup(quotes)
 
     for tag in parsed_html.find_all("li", recursive=True):
-        print("'{}':{}".format(tag.text, tag.contents[0].get("value")))
+        print("'{}':{},".format(tag.text, tag.contents[0].get("value")))
 
 
 if __name__ == "__main__":
